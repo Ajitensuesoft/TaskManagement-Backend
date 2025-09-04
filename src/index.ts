891f34fbefa0d955b15b3da43690077eaf0a4e11
Ajit Kumar <221511084+Ajitensuesoft@users.noreply.github.com>
2025-09-04 18:27:48 +0530

    Minor change

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const app = express();
 // });
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin:"https://taskmanagment-frontend-six.vercel.app/",
+  origin:"https://taskmanagment-frontend-six.vercel.app",
   credentials: true
 }))
 app.get("/", (req: Request, res: Response) => {
