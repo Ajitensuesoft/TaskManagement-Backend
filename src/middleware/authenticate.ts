83802078424388257c893ea057ea23adbabb0f01
Ajitensuesoft <ajit.kumar@ensuesoft.com>
2025-09-05 13:29:41 +0530

    Update authenticate.ts

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
-dotenv.config();
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
+// dotenv.config();
+// const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
 export const authenticates = (req: any, res: any, next: Function) => {
   const token = req.cookies.token; // cookie name from login
 
@@ -9,7 +9,7 @@ export const authenticates = (req: any, res: any, next: Function) => {
   }
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET_KEY as string) as { id: string };
+    const decoded = jwt.verify(token,'AJIT' as string) as { id: string };
     console.log("decoded",decoded);
     req.userId = decoded.id; // ✅ use _id from token and set userId
     console.log(req.userId = decoded.id);
